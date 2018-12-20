0c457eb592aaf7a0321073ded6bd025708090cb0
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-12-20 07:21:40 -0300

    Add authentication interfaces module

---
+++ webapps-commons-modules/webapps-commons-authentication-interfaces/src/main/java/com/bellotapps/webapps_commons/security/authentication/TokenData.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2018 BellotApps
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bellotapps.webapps_commons.security.authentication;
+
+import com.bellotapps.webapps_commons.security.authorization.Grant;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * A wrapper class that encapsulates information taken from a token.
+ */
+public final class TokenData {
+
+    /**
+     * The token's id.
+     */
+    private final long id;
+
+    /**
+     * The token's owner username.
+     */
+    private final String username;
+
+    /**
+     * The grants given to the token.
+     */
+    private final List<Grant> roles;
+
+    /**
+     * @param id       The token's id.
+     * @param username The token's owner username.
+     * @param roles    The grants given to the token.
+     */
+    public TokenData(final long id, final String username, final List<Grant> roles) {
+        this.id = id;
+        this.username = username;
+        this.roles = Collections.unmodifiableList(roles); // This list cannot be changed.
+    }
+
+    /**
+     * @return The token's id.
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @return The token's owner username.
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @return The grants given to the token.
+     */
+    public List<Grant> getGrants() {
+        return roles;
+    }
+}
---
+++ webapps-commons-modules/webapps-commons-authentication-interfaces/src/main/java/com/bellotapps/webapps_commons/security/authentication/TokenException.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2018 BellotApps
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bellotapps.webapps_commons.security.authentication;
+
+import com.bellotapps.webapps_commons.exceptions.UnauthenticatedException;
+
+/**
+ * Exception to be thrown when there is any problem with a token (i.e decoding, invalid, blacklisted, etc.).
+ */
+public class TokenException extends UnauthenticatedException {
+
+    /**
+     * Default constructor.
+     */
+    public TokenException() {
+        super();
+    }
+
+    /**
+     * Constructor which can set a {@code message}.
+     *
+     * @param message The detail message, which is saved for later retrieval by the {@link #getMessage()} method.
+     */
+    public TokenException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Constructor which can set a mes{@code message} and a {@code cause}.
+     *
+     * @param message The detail message, which is saved for later retrieval by the {@link #getMessage()} method.
+     * @param cause   The cause (which is saved for later retrieval by the {@link #getCause()} method).
+     *                For more information, see {@link RuntimeException#RuntimeException(Throwable)}.
+     */
+    public TokenException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+}
