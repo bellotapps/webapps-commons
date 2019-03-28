7dd529afd71b3e5e06276fde859fd232df2172a2
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-03-28 14:36:37 -0300

    Create a NotImplementedException

---
+++ webapps-commons-modules/webapps-commons-exceptions/src/main/java/com/bellotapps/webapps_commons/exceptions/NotImplementedException.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019 BellotApps
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bellotapps.webapps_commons.exceptions;
+
+/**
+ * {@link RuntimeException} thrown when trying to invoke an operation that is not implemented yet.
+ */
+public class NotImplementedException extends RuntimeException {
+
+    /**
+     * Default constructor.
+     */
+    public NotImplementedException() {
+        super();
+    }
+
+    /**
+     * Constructor which can set a {@code message}.
+     *
+     * @param message The detail message, which is saved for later retrieval by the {@link #getMessage()} method.
+     */
+    public NotImplementedException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructor which can set a mes{@code message} and a {@code cause}.
+     *
+     * @param message The detail message, which is saved for later retrieval by the {@link #getMessage()} method.
+     * @param cause   The cause (which is saved for later retrieval by the {@link #getCause()} method).
+     *                For more information, see {@link RuntimeException#RuntimeException(Throwable)}.
+     */
+    public NotImplementedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
